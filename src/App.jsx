c7e7662d9be334e203ac521b3901370d73d05bd1
piyush goyal <piyush2022ug@gmail.com>
2025-05-17 11:41:56 +0530

    feat: routing and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Edit from "./pages/Edit";
+import Post from "./pages/Post";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+import PrivateRoute from "./components/PrivateRoute";
+// import { AuthProvider } from "./contexts/authContext";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    // <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/editor" element={<Edit />} />
+            <Route path="/editor/:id" element={<Edit />} />
+          </Route>
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile/:username" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    // </AuthProvider>
+  );
 }
