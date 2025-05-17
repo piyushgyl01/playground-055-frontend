096846404e99f14f5bb6f649b1315f955761255d
piyush goyal <piyush2022ug@gmail.com>
2025-05-17 11:47:46 +0530

    feat: useFetch hook for data fetching

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
-export default function useFetch() {
-  return (
-    <div>useFetch</div>
-  )
+export default function useFetch(url, initialData) {
+  const [data, setData] = useState(initialData);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchData() {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      setError(error.message);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { data, error, loading, refetch: fetchData };
 }
